5af9dc6ee9b1b49841c335bd8cfbe88395bc9df2
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-10-09 15:11:03 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZmLtky9STCrucJ4LmI97R5arQtpnrExE",
+  authDomain: "emperiortech.firebaseapp.com",
+  projectId: "emperiortech",
+  storageBucket: "emperiortech.appspot.com",
+  messagingSenderId: "804593623282",
+  appId: "1:804593623282:web:2edd3f2ddc521abc4f706b",
+  measurementId: "G-7KTFY72644"
+};
+
+export default firebaseConfig;
